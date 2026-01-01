9157088ae48e4ddb94adbf8a7d8bff92bbf98d94
Gibsy <gibsy@example.com>
2026-01-01 20:26:54 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+import json
+import requests
+from datetime import datetime, timezone
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")                        # neocities API key
+SITE = os.getenv("SITE")                              # neocities login
+STATUS_FILE = "status.json"
+INTERVAL = 30
+
+start_time = datetime.now(timezone.utc)
+
+def upload_status(online=True):
+    now = datetime.now(timezone.utc)
+    data = {
+        "online": online,
+        "last_update": now.isoformat(),
+        "online_since": start_time.isoformat()
+    }
+
+    with open(STATUS_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+    with open(STATUS_FILE, "rb") as f:
+        files = {"status.json": f}
+        headers = {"Authorization": f"Bearer {API_KEY}"}
+        r = requests.post("https://neocities.org/api/upload", headers=headers, files=files)
+        print(f"[{now}] Uploaded status.json, response code: {r.status_code}")
+
+if __name__ == "__main__":
+    while True:
+        try:
+            upload_status(True)
+        except Exception as e:
+            print("Error uploading:", e)
+        time.sleep(INTERVAL)
---
+++ readme.md
@@ -0,0 +1,39 @@
+# WebPC Status
+
+This is a simple project to track the status of a PC and display it on a Neocities web page.  
+The Python script updates `status.json` on Neocities, and the HTML page reads this file to show whether the PC is online or offline.
+
+---
+
+## Features
+
+- Python script `main.py` updates status every 30 seconds.
+- Example HTML page (`example.html`) shows:
+  - Online/Offline status
+  - Time since PC online/offline
+  - Offline is detected if the status hasn't updated for more than 1 minute.
+- Easy to use, lots of room for improvement!
+
+---
+
+## Quick guide
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/Gibsy/WebPCStatus.git
+cd WebPCStatus 
+```
+### 2. Install dependencies
+```bash
+pip install -r requirements.txt
+```
+### 3. Create a .env file in the project root
+```bash
+API_KEY=YOUR_NEOCITIES_API_KEY
+SITE=YOUR_SITE_NAME
+```
+### 4. Run the Python script
+```bash
+python main.py
+```
