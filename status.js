40206df637a0fdafeee969281a730abeb4fe3b63
gibsy <100948491+Gibsy@users.noreply.github.com>
2026-03-19 13:41:23 +0500

    Update status.js

--- status.js
+++ status.js
@@ -2,15 +2,18 @@ async function updateStatus() {
     try {
         const res = await fetch("status.json?_=" + Date.now());
         const data = await res.json();
+
         const now = Date.now();
         const lastUpdate = new Date(data.last_update).getTime();
         const onlineSince = new Date(data.online_since).getTime();
+
         const diffLastMs = now - lastUpdate;
-        const isOnline = diffLastMs <= 240_000;
+        const isOnline = diffLastMs <= 240000;
 
         let diffMs;
         let label;
         let color;
+
         if (isOnline) {
             diffMs = now - onlineSince;
             label = "Online for";
@@ -24,6 +27,7 @@ async function updateStatus() {
         const diffMin = Math.floor(diffMs / 60000);
         const h = Math.floor(diffMin / 60);
         const m = diffMin % 60;
+
         let timeText = "";
         if (h > 0) timeText += `${h}h `;
         timeText += `${m}min`;
